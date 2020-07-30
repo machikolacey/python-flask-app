bc039ff8ece15dcb795ceabea19844399e0fcc3b
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-07-30 23:06:10 +0000

    pushing latest changes - add task function done

--- app.py
+++ app.py
@@ -9,13 +9,23 @@ app.config["MONGO_DBNAME"] = "task manager"
 app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster.1nsni.mongodb.net/task_manager?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
-
-
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
